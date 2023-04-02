259cc3e8487b13831681da0240f321cf5471699b
Atrolide <loszewskigrzegorz@gmail.com>
2023-04-02 13:32:27 +0200

    Update brute_force.py

--- Assignment1/Task1/pattern_matching/brute_force.py
+++ Assignment1/Task1/pattern_matching/brute_force.py
@@ -1,11 +1,17 @@
 def brute_force_search(pattern, text):
+    # Create an empty list to store the starting positions of the pattern in the text
     positions = []
+
+    # Iterate over all possible starting positions i in the text where the length of the substring starting at i is at least as long as the pattern
     for i in range(len(text) - len(pattern) + 1):
+        # Initialize a counter j to 0
         j = 0
+        # While j is less than the length of the pattern and the character at position i + j in the text matches the character at position j in the pattern, increment j
         while j < len(pattern) and text[i + j] == pattern[j]:
             j += 1
+        # If j equals the length of the pattern, the pattern has been found at position i in the text. Append i to the positions list.
         if j == len(pattern):
             positions.append(i)
 
-    print('This is the Brute Force Algorithm!')
+    # Return the list of starting positions of the pattern in the text
     return positions
