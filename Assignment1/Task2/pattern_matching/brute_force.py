21219b8addbbb45348fe9245735600d2ebab4d81
Atrolide <loszewskigrzegorz@gmail.com>
2023-04-24 19:02:28 +0200

    Wildcard escape fix

--- Assignment1/Task2/pattern_matching/brute_force.py
+++ Assignment1/Task2/pattern_matching/brute_force.py
@@ -1,27 +1,31 @@
-def brute_force_search(pattern, text):
-    n = len(text)
-    m = len(pattern)
-    i = 0
-    while i <= n - m:
-        j = 0
-        while j < m and (text[i+j] == pattern[j] or pattern[j] == '?' or pattern[j] == '*'):
-            if pattern[j] == '\\':
-                j += 1
+def brute_force_search (pattern, text):
+    m, n = len(text), len(pattern)
+    i, j = 0, 0
+    while i < m and j < n:
+        if pattern[j] == '\\':
             j += 1
-        if j == m:
-            return True
-        if pattern[j:j+1] == '*':
-            k = i + j
-            while k <= n:
-                if brute_force_search(pattern[j+1:], text[k:]):
-                    return True
-                k += 1
-        i += 1
-    return False
-
-
-
-
-
-
-
+            if j == n:
+                return False
+            if text[i] != pattern[j]:
+                return False
+        elif pattern[j] == '?':
+            i += 1
+            j += 1
+        elif pattern[j] == '*':
+            j += 1
+            if j == n:
+                return True
+            while i < m and text[i] != pattern[j] and pattern[j] != '?':
+                i += 1
+            if i == m:
+                return False
+        elif text[i] != pattern[j]:
+            return False
+        else:
+            i += 1
+            j += 1
+    if j == n and i == m:
+        return True
+    if j < n and pattern[j] == '*':
+        j += 1
+    return j == n and i == m
