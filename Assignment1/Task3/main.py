f32577eaa5853a0ce26c6ad2ce90d7ed0847a650
xd4niel <danyxd01@gmail.com>
2023-05-10 01:13:23 +0200

    added comments

--- Assignment1/Task3/main.py
+++ Assignment1/Task3/main.py
@@ -2,34 +2,51 @@ from rabin_karp_picture import rabin_karp_duplicate_picture
 from random import randint
 from time import time
 
+# Define a function named generate_picture that generates a random picture of size M x N.
 def generate_picture(M, N):
+    # Use a nested list comprehension to generate a random picture of size M x N.
     return [[randint(0, 255) for _ in range(N)] for _ in range(M)]
 
+# Define a function named duplicate_corner that checks if there is a duplicated corner of size K in a given picture.
 def duplicate_corner(picture, K):
+    # Extract the top-right corner of size K from the picture.
     top_right_corner = [row[-K:] for row in picture[:K]]
+    # Iterate over the remaining rows in the picture.
     for i in range(K, len(picture)):
+        # If the top-right corner matches the top-left corner of the current row, then there is a duplicated corner.
         if picture[i-K][:K] == top_right_corner:
             return True
+        # Iterate over the remaining columns in the current row.
         for j in range(K, len(picture[0])):
+            # Check if the current KxK sub-picture matches the top-right corner.
             if all(picture[i-k][j-K+jj] == top_right_corner[k][jj] for k in range(K) for jj in range(K)):
                 return True
+    # If no duplicated corner is found, return False.
     return False
 
+# Define a function that generates a random picture and tests if the duplicate_corner function and the rabin_karp_duplicate_picture function return the same result.
 def test_algorithm(M, N, K):
+    # Generate a random picture of size M x N.
     picture = generate_picture(M, N)
 
+    # Measure the time it takes to run the duplicate_corner function on the picture.
     start_time = time()
     expected_result = duplicate_corner(picture, K)
     expected_time = time() - start_time
 
+    # Measure the time it takes to run the rabin_karp_duplicate_picture function on the picture.
     start_time = time()
     result = rabin_karp_duplicate_picture(picture, K)
     actual_time = time() - start_time
 
+    # Assert that the result of the rabin_karp_duplicate_picture function matches the expected result of the duplicate_corner function.
     assert result == expected_result, f"Error: expected {expected_result}, but got {result}"
+    # Assert that the actual time it took to run the rabin_karp_duplicate_picture function is less than or equal to 10 times the time it took to run the duplicate_corner function.
     assert actual_time <= expected_time * 10, f"Error: algorithm took too long ({actual_time} seconds)"
+    # Print a success message if both assertions pass.
     print("Test passed successfully!")
 
+# If the module is being run as the main program, run three tests on pictures of different sizes.
 if __name__ == "__main__":
     test_algorithm(10, 10, 3)
     test_algorithm(100, 100, 5)
--- Assignment1/Task3/rabin_karp_picture.py
+++ Assignment1/Task3/rabin_karp_picture.py
@@ -1,25 +1,43 @@
 def rabin_karp_duplicate_picture(picture, K):
-    # Calculate the hash value of the pattern
+    # Initialize pattern_hash as 0
     pattern_hash = 0
+    # Loop through rows of the first K columns
     for i in range(K):
+        # Loop through columns of the last K rows
         for j in range(K):
+            # Add the hash value of the current pixel to pattern_hash
+            # Hash value is calculated by shifting the pixel value by a position based on its row and column
             pattern_hash |= (picture[i][len(picture[0])-K+j] << ((K-i-1)*K + (K-j-1)))
 
-    # Calculate the hash value of each row
+    # Initialize an empty list to store row hashes
     row_hashes = []
+    # Initialize prev_row_hash as 0
     prev_row_hash = 0
+    # Loop through rows of the picture
     for row in picture:
+        # Initialize row_hash as 0
         row_hash = 0
+        # Loop through columns of the last K rows
         for j in range(K):
+            # Add the hash value of the current pixel to row_hash
+            # Hash value is calculated by shifting the pixel value by a position based on its column
             row_hash |= (row[len(row)-K+j] << (K-j-1))
+        # XOR the previous row hash with the current row hash and store the result as the current row hash
         row_hash ^= prev_row_hash
+        # Append the current row hash to the list of row hashes
         row_hashes.append(row_hash)
+        # Update prev_row_hash by shifting it right by 1 and adding the current row hash shifted left by K-1
         prev_row_hash = (prev_row_hash >> 1) | (row_hash << (K-1))
 
-    # Check for duplicates
+    # Create a list of the first K rows of the picture, each truncated to the last K columns
     pattern = [row[-K:] for row in picture[:K]]
+    # Loop through the row hashes, starting from the first row and ending at the row that is K rows from the bottom
     for i, row_hash in enumerate(row_hashes[:len(picture)-K+1]):
+        # If the current row hash ANDed with a mask of K*K 1's is equal to pattern_hash, check if the pattern matches the corresponding pixels in the picture
         if row_hash & ((1 << (K*K)) - 1) == pattern_hash:
+            # Loop through the pixels in the pattern and check if they match the corresponding pixels in the picture
             if all(pattern[m][n] == picture[i+m][n+len(picture[0])-K] for m in range(K) for n in range(K)):
+                # If the pattern matches, return True
                 return True
+    # If no matching pattern is found, return False
     return False
