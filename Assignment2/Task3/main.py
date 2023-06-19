f9ec8ef87865c9e25bf07ae748bf2526a0e1fadd
Adam Kuszczyński <68527324+juhum@users.noreply.github.com>
2023-06-19 19:16:25 +0200

    Update main.py

--- Assignment2/Task3/main.py
+++ Assignment2/Task3/main.py
@@ -85,5 +85,5 @@ def compare_search_insert_times(load_factors):
 
 # Compare search/insert times for different load factors
 # load_factors = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3]
-load_factors = np.arange(0.1, 2.5 + 0.1, 0.1)
+load_factors = np.arange(0.1, 1.1, 0.1)
 compare_search_insert_times(load_factors)
